a988ee3ee96580f1f559042384fe005dbc5bec25
emonShimoul <emonshimoul@gmail.com>
2024-09-21 16:20:55 +0600

    footer start

---
+++ footer.php
@@ -0,0 +1,16 @@
+    <div class="footer">
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class="footer-left w-50">
+                    <p>+8801711843533 | lpgactive@gmail.com
+                    Dhaka, Bangladesh</p>
+                    <p>Copyright © 2020 Active LPG. All Rights Reserved.</p>
+                </div>
+                <div class="footer-right w-50">
+                    <p>Advocating for Bangladesh businesses since 1984. Headquartered in Dhaka, Bangladesh, Maple Leaf International represents world-class designers, manufacturers, and suppliers from all over the world.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
