267b4b41e97b6a944f64aa1348b90429683e1125
emonShimoul <emonshimoul@gmail.com>
2024-09-21 16:36:48 +0600

    footer completed

--- header.php
+++ header.php
@@ -2,8 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
-    <style>
-        
-    </style>
 </head>
