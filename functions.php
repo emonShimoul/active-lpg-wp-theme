453323796752fe773afe5a55e757452c74e361c3
emonShimoul <emonshimoul@gmail.com>
2024-09-24 14:09:00 +0600

    working with gutenberg block

--- functions.php
+++ functions.php
@@ -11,8 +11,16 @@ function activelpg_bootstrapping(){
         "height" => '100'
     );
     add_theme_support("custom-logo", $alpha_custom_logo_defaults);
+
+
+    // for gutenberg
+    add_theme_support( 'wp-block-styles' );
+    add_theme_support( 'align-wide' );
+    add_theme_support( 'editor-styles' );
+    add_editor_style( 'style-editor.css' ); // The file name can be customized if needed
 }
 add_action("after_setup_theme", "activelpg_bootstrapping");
+
 
 function activelpg_assets(){
     wp_enqueue_style("activelpg", get_stylesheet_uri());
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <body <?php body_class(); ?>>
     <?php get_template_part("navbar"); ?>
 
+    <?php if(is_front_page()){ ?>
+
     <div class="heading">
         <p>Active LPG</p>
         <h1>Unleash the Power <br> of LPG Conversion</h1>
@@ -75,15 +77,17 @@
 
     <div class="container text-center mb-4">
         <img src="<?php echo header_image(); ?>" alt="">
-    </div>
+    </div> 
+            
+    <?php } ?>
 
     <div class="posts">
-        <?php
-            while(have_posts()){
-                the_post();
-                get_template_part("post-formats/content",get_post_format());
-            }
-        ?>
+    <?php
+        while(have_posts()){
+            the_post();
+            get_template_part("post-formats/content",get_post_format());
+        }
+    ?>
     </div>
 
 <?php get_footer(); ?>
